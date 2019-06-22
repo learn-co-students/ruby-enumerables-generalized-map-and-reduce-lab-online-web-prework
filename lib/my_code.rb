cca8a0ba32c28ef191315a06f4914392a8d4a362
Dominique De León <dominique.a.deleon@gmail.com>
2019-06-22 20:29:28 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,19 @@ def map(arr)
   end
   new_arr
 end
+
+def reduce(arr, start = nil)
+  # no idea what's even going on here
+  if start
+    accum = start
+    i = 0
+  else
+    accum = arr[0]
+    i = 1
+  end
+  while i < arr.length
+    accum = yield(accum, arr[i])
+    i += 1
+  end
+  accum
+end
