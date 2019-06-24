0e4603eb93b169ed2ea10cd6e32f714bf8356167
github username <github email address>
2019-06-24 07:45:12 -0400

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,28 +11,29 @@ end
 
 def reduce(arr, start = nil) # get them set up with correct parameters first
 
-  #start them off here, it will pass first reduce test:
-  
-  running_total = arr[0]     #grab first item, that's where you start adding
-  i = 1             #start counting at i; only need to add next item, have accounted for first
-  
+  ##########  start them off here, it will pass first reduce test: ###########
+  running_total = arr[0]   # grab first item, that's where you start adding
+  i = 1 # start counting at i; only need to add next item, have accounted for first
+
   while i < arr.length
-  running_total = yield(running_total, arr[i]) # easist to explain this bit to them as far as the addition block goes (first two tests); can go into the details of the last four tests after passing them; if you like
-  i += 1 # keep incrementing
+    running_total = yield(running_total, arr[i]) # easiest to explain this bit to them as far as the addition tests go (first two tests); can go into the details of the last four tests after passing them; if you like
+    i += 1 # keep incrementing
   end
-  running_total 
-  
-  # to pass the second test (and susbsequently pass the last four), you'll need to add conditional logic to handle when a starting point is given
-  
+  running_total
+
+  ##### adjust code above to pass the second test (and subsequently pass the last four) #####
+  ##### you'll need to add conditional logic to handle when a starting point is given #####
+
   if start
     running_total = start
-    i = 0 # if a start is given, you want to start at the beginning of the array when adding
+    i = 0   # if a start is given, you want to start at the beginning of the array when adding
   else
-    running_total = arr[0] # if a start is not given, the starting point is first item in the array
-    i = 1 # so you start at the second item when adding
+    running_total = arr[0]   # if a start is not given, the starting point is the first item in the array
+    i = 1   # so you start at the second item when adding
   end
+
   while i < arr.length
-    running_total = yield(running_total, arr[i]) # you are yielding to a block that contains a rule, but you need to provide starting point and next item to add
+    running_total = yield(running_total, arr[i]) # you are yielding to a block that contains a rule (addition or whatever), but you need to provide starting point and next item to add
     i += 1
   end
   running_total
