1be1bec6046a1a694e9725b51cd51dfdb48f0c22
Doug Kintop <dkintop@hotmail.com>
2019-07-03 23:05:37 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,40 @@
-# Your Code Here
+def map(array)
+  new_array = []
+   array.length.times do |index|
+    b = yield (array[index])
+    new_array << b
+    end
+
+new_array
+end
+
+
+
+
+
+
+
+#closest yet. having difficulty with boolean aspect.
+def reduce (source_array, starting_point = nil)
+
+if starting_point
+total= starting_point
+i= 0
+else
+  total= source_array[0]
+  i=1
+end
+
+while source_array.length > i 
+  
+  total= yield(total, source_array[i])
+  i= i+1
+  end
+total
+end
+
+
+
+
+
+    
