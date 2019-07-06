b05be644b9c5faeb3220914f69b76a00723a7702
Audrea Cook <audreacwork@gmail.com>
2019-07-06 13:43:27 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,12 +10,20 @@ def map(array)
   new_array
 end
 
+
 def reduce(array, value = nil)
- # I need to ask for help. I don't understand how I got the solution before. I probably peeked.
+ if value
+   total = value
+   counter = 0
+ else
+   total = array[0]
+   counter = 1
+ end
   while counter < array.length do
-    total += yield(array[counter])
+    # math stuff is happening in the block given by specs
+    total = yield(total, array[counter])
     counter += 1
   end
-  
+
   total
 end
