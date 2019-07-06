815994e35f20cd178d64299e3f70938e6bd219f3
Pablo Gonzalez <pabgon0@gmail.com>
2019-07-06 03:39:28 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
-# Your Code Here
+def map(array)
+  new_array = []
+  i = 0
+    while i < array.length
+      new_array.push(yield(array[i]))
+      i += 1
+    end
+  new_array
+end
+
+def reduce(array,start = nil)
+  if start
+    new_array = start
+    i = 0
+  else
+    new_array = array[0]
+    i = 1
+  end
+  
+  while i < array.length
+    new_array = yield(new_array, array[i])
+    i += 1
+   end
+  new_array
+end
