af8786b6c1533fc0a900305dd59740a0a6e39066
Jenny O'Hara <jenny.s.ohara@gmail.com>
2019-07-06 15:09:12 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,12 @@
 # Your Code Here
+def generalized_map(array)
+  new =[]
+  i = 0
+  while i < array.length do
+    yield
+    i += 1
+  end
+  return new
+end
+
+generalized_map(array) { |n| n * -1 }
