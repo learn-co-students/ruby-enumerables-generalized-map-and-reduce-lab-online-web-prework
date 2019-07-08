c2aea543e85dbe9c68895273ca2690a9dc94710c
assane savadogo <assane.savadogo81@gmail.com>
2019-07-08 14:45:52 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
-# Your Code Here
+require 'pry'
+def map(array)
+  new_array = []
+  i = 0 
+  while i < array.size do 
+   new_array.push(yield(array[i]))
+   i += 1 
+   end 
+ new_array
+end
+
+
+
+def reduce(source_array, value = nil)
+ if value 
+    x = value 
+   i = 0 
+  else
+   x = source_array[0]
+   i = 1
+  end
+  while i < source_array.length do 
+    #binding.pry
+    x = yield(x, source_array[i])
+    i += 1
+    end
+  return x
+end
