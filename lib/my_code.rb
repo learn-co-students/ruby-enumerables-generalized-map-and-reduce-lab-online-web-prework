b264ed0b6c10a389b8933761982abc3e0c52865e
Jenny O'Hara <jenny.s.ohara@gmail.com>
2019-07-08 14:23:13 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,12 +1,21 @@
 # Your Code Here
-def generalized_map(array)
+def map(array)
   new =[]
   i = 0
   while i < array.length do
-    yield
+    result = yield(array[i])
+    new << result
     i += 1
   end
   return new
 end
 
-generalized_map(array) { |n| n * -1 }
+def reduce(source_array, starting_point = 0)
+      reduce_total = 0
+      i = 0
+      while i < source_array.length do      
+       reduce_total = yield(source_array, starting_point)
+        i += 1
+      end
+  reduce_total
+end
