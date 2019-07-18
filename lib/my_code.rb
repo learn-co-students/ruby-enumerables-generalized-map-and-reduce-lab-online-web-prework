25ea17cd90565b1452b0ff2cdeb4af9a14896647
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-07-18 05:29:17 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,19 @@
 # Your Code Here
 
-def map (ele)
+def map (array)
   
-  yield ele
+  new_array = []
+  i = 0
+  
+  while i < array.length 
+  
+  new_array.push(yield(array[i]))
+  
+  i += 1
+  end
+
+  new_array
   
 end
 
-map(ele) {|n| n *= -1}
+#array.map {|n| n * -1}
