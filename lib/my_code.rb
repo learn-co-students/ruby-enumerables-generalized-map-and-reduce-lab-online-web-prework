926ce1844e0edc77a7ae5200cb6d766efc1360b2
Christian Cain <christian22cain@yahoo.com>
2019-07-22 00:02:40 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def map(array)
+  result_array = []
+  i = 0
+  
+  while i < array.length do
+    result_array[i] = yield(array[i]) #unique work
+    i += 1
+  end
+  
+  return result_array
+end
+
+def reduce(array, starting_point = 0)
+  result = starting_point
+  i = 0
+  
+  while i < array.length do
+    result = yield(result, array[i])
+    i += 1
+  end
+  
+  return result == 0 ? true : result
+  
+end
