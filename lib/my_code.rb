5aff4144edf2912a78f477cd5fa16b388cc75a9e
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-07-24 04:02:38 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,9 @@
-# Your Code Here
+(map([1, 2, 3, -9]){|n| n * -1}) .to eq ([-1, -2, -3, -9])
+(map(dune){|n| n}) .to eq(dune)
+(map([1, 2, 3 , -9]){|n| * 2}) .to eq [2, 4, 6, -18])
+(map([1, 2, 3, -9]){|n| n * .to eq ({1, 4, 9, 81})
+(reduce(source_array, starting_point){|memo, n| memo + n}) .to eq(106)
+(reduce(source_array){|memo,n|memo && n}) .to be_truthy
+(reduce(source_array){|memo, n| memo && n}) .to be_falsy
+(reduce(source_array){|memo, n|memo||n}) .to eq(true)
+(reduce(source_array){|memo, n| memo && n}) .to eq(false)
