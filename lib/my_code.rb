d33e1724e3b699b0221d6facc805bc17cf2019ba
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-07-26 21:06:21 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,9 @@
-# Your Code Here
+def make_sandwich(elelment1, element2)
+  base = "A #{elelment1} and #{element2}"
+  puts base
+  yield
+  base
+end
+
+make_sandwich("chicken", "a sense of malaise") do |innards|
+  puts "making some tasty stuff..."
