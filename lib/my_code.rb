498935bf67960ae394529217b820b09f2c7b6d97
Srikalyan Megati <kalmegati@gmail.com>
2019-08-03 03:54:31 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,10 +6,17 @@ def map(source_array)
 end
 
 
-def reduce(source_array, starting_point = 0)
-  reduction = starting_point
-  source_array.length.times do |index|
-    reduction = yield(reduction, source_array.pop)
+def reduce(source_array, starting_point = nil)
+  if starting_point
+    reduction = starting_point
+    counter = 0
+  else
+    reduction = source_array[0]
+    counter = 1
+  end
+  while counter < source_array.length do
+    reduction = yield(reduction, source_array[counter])
+    counter += 1
   end
   reduction
 end
