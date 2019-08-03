72ad0b8eb991238d5ddbce1005f8ea6b75584a08
Srikalyan Megati <kalmegati@gmail.com>
2019-08-03 03:23:48 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,15 @@
-# Your Code Here
+def map(source_array)
+  source_array.length.times do |index|
+    source_array[index] = yield(source_array[index])
+  end
+  source_array
+end
+
+
+def reduce(source_array, starting_point = 0)
+  reduction = starting_point
+  source_array.length.times do |index|
+    reduction = yield(reduction, source_array.pop)
+  end
+  reduction
+end
