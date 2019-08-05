f315063c328907bed0e04334a1d00c1f4e09befc
J K <kim.ju652@gmail.com>
2019-08-05 06:16:30 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,19 @@
 # Your Code Here
+def map(array)
+  new = []
+  for x in array do
+    new.push(yield(x))
+  end 
+  return new
+end
+
+def reduce(array, starting = 0)
+  for x in array do
+    y = yield(x, starting)
+    starting = y
+  end
+  if y == nil
+      return false
+  end
+  return y
+end
