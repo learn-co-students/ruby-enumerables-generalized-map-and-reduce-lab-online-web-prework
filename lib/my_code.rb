b1e1f827a2bcd528d587180387dcfba6719486ab
Chaya Deaver <chayadeaver@gmail.com>
2019-08-06 18:54:20 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def map(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+  new.push(yield(source_array[i]))
+  i+=1
+  end
+  return new
+end
+  
+def reduce(source_array,starting_point=nil)
+  if starting_point
+    memo = starting_point
+    i = 0 
+  else
+    memo = source_array[0]
+    i = 1 
+  end
+    while i < source_array.length do
+      memo = yield(memo, source_array[i])
+      i+=1
+    end
+    memo
+end
