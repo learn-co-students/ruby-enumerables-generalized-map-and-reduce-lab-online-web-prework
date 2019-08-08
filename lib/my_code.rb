b451708f1333b1b15ccefe945993edcfb28f6cd4
James Mobus <mobusj@live.com>
2019-08-08 14:01:35 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
 # Your Code Here
+def map(array)
+  array.length.times do |i|
+    array[i] = yield(array[i])
+  end
+  
+  array
+end
+
+
+def reduce(array, ret = 0)
+  array.length.times do |i|
+    ret = yield(ret, array[i])
+  end
+  
+  if ret === 0 then
+    ret = true
+  end
+  
+  ret
+end
