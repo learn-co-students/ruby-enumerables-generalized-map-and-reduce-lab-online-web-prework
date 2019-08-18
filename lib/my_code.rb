d0777f46ad3c51bd3914e3c3a569ecd772b0cfdf
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-18 15:49:08 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,26 +1,26 @@
 # Your Code Here
 
 def map(array)
-  new = []
-  counter = 0 
-  while counter < array.length do 
-    new.push(yield(array[counter])) 
-    counter +=1 
+  new_array = []
+  i = 0 
+  while i < array.length do 
+    new_array.push(yield(array[i])) 
+    i += 1 
   end 
-  return new  
+  return new_array
 end 
 
-def reduce(source_array,start=nil)
+def reduce(source_array, start=nil)
   if start 
-    counter = 0 
+    i = 0 
     total = start
   else 
     total = source_array[0]
-    counter = 1 
+    i = 1 
   end
-  while counter < source_array.length do
-    total = yield(total,source_array[counter])
-      counter +=1
+  while i < source_array.length do
+    total = yield(total, source_array[i])
+      i += 1
   end 
   total
 end 
