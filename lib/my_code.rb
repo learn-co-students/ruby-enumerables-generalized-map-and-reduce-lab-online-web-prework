f02c333ae18ef790a829775aee5015b302725122
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-18 02:51:25 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
 # Your Code Here
+
+def map(array)
+  new = []
+  counter = 0 
+  while counter < array.length do 
+    new.push(yield(array[counter])) 
+    counter +=1 
+  end 
+  return new  
+end 
+
+def reduce(source_array,start=nil)
+  if start 
+    counter = 0 
+    total = start
+  else 
+    total = source_array[0]
+    counter = 1 
+  end
+  while counter < source_array.length do
+    total = yield(total,source_array[counter])
+      counter +=1
+  end 
+  total
+end 
