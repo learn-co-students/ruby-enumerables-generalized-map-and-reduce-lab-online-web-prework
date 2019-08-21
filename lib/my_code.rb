d4873d2c7f77409e0ec7706b850e1cbda8d4abe0
Shamel Lakin <christlife4u@gmail.com>
2019-08-21 15:11:04 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,10 @@
-# Your Code Here
+def neg_num ([1, 2, 3, -9])
+  map { |n| n * -1 }
+end
+p neg_num ([1, 2, 3, -9])
+
+def map_to_square([1, 2, 3, -9])
+  map { |n| n * 2}
+end
+p map_to_square ([1, 2, 3, -9])
+
