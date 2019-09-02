47e23c2e399f4e672b603167d4a22d5d5856585d
Brianna Jordan <bryvjordan9@gmail.com>
2019-09-02 19:37:38 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,24 @@
-def my_own_map(array)
-  array = [1, 2, 3, -9]
+def map(b)
+  new = []
   n = 0
-  while n < array.length do
-    array.push(yield(array[n]))
+  while n < b.length do
+    new.push(yield(b[n]))
     n += 1
   end
-  array
+  new
+end
+
+def reduce(b, sp=nil)
+  if sp
+    accum = sp
+    i = 0
+  else
+    accum = b[0]
+    i = 1
+  end
+  while i < b.length
+    accum = yield(accum, b[i])
+    i += 1
+  end
+  accum
 end
