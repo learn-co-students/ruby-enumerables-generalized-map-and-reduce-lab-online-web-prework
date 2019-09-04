70fc908d9aecca89ba8d64bf71e9a237591d9706
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-04 19:29:20 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,43 @@
-# Your Code Here
+#map
+
+def map(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push(yield(source_array[i]))
+    i += 1
+  end
+  return new
+end
+
+
+#reduce
+
+#def reduce_to_total(source_array, starting_point=0)
+ # total = starting_point
+ # i = 0
+ # while i < source_array.length do
+ #   total += source_array[i]
+ #   i += 1
+ # end
+ # return total
+#end
+
+def reduce(source_array, starting_point=nil)
+  
+  if starting_point
+    i = 0
+    output = starting_point
+  else
+    i = 1
+    output = source_array[0]
+  end
+      
+    while i < source_array.length do
+      output = yield(output, source_array[i])
+      i += 1
+    end
+    return output  
+end
+
+# reduce([1,2,3,4]) {|memo, n| memo && n}
