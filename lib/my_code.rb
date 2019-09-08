c08c73bafe314d6c77c3a5c535252276f21951ae
William Stevens <wstevens.hornet@gmail.com>
2019-09-08 20:24:40 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,31 @@
 # Your Code Here
+
+def map(source_array) #Finally I get it. The test is using its own logic and this just applies it
+  new_array = []
+  i = 0 
+  while i < source_array.length 
+    new_array.push(yield(source_array[i]))
+    i += 1
+  end
+  new_array
+end
+
+def reduce(source_array, string_val=nil)
+  if string_val
+    sum = string_val
+    i = 0
+  else
+    sum = source_array[0]
+    i = 1
+  end
+  while i < source_array.length
+    sum = yield(sum, source_array[i])
+    i += 1
+  end
+  sum
+end
+
+
+
+
+
