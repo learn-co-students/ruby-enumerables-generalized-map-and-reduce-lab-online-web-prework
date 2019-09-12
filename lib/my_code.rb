36e8bccf4c993b54196a844dfe4cb23c4267bd0d
Melissa Christman <melchrstmn@gmail.com>
2019-09-12 23:19:06 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def map(source_array)
+  new_array = []
+  i = 0 
+  while i < source_array.length 
+    new_array.push(yield(source_array[i]))
+    i += 1
+  end
+  new_array
+end
+
+def reduce(source_array, string_val=nil)
+  if string_val
+    sum = string_val
+    i = 0
+  else
+    sum = source_array[0]
+    i = 1
+  end
+  while i < source_array.length
+    sum = yield(sum, source_array[i])
+    i += 1
+  end
+  sum
+end
