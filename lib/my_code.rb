a171ef01e8c1cc9a1bac348e0fc3e99157c48d64
Kathleen Kamali <kathleenkamali@gmail.com>
2019-09-15 20:27:12 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
-# Your Code Here
+def map(source_array) 
+  counter = 0 
+  new_array = []
+  while counter < source_array.length do
+    new_array << yield(source_array[counter])
+    counter += 1
+  end
+  new_array
+end
+
+def reduce(source_array, starting_point = 0)
+  counter = 0
+  total = starting_point
+  while counter < source_array.length do
+    total = yield(source_array[counter], total)
+    counter += 1
+  end
+  if total == nil 
+    return false
+  end
+  total
+end
