c78369f1d67b5013fc304821d985118d99ae9717
Juan Bermudez <juan.bermudez1102@outlook.com>
2019-09-22 23:11:50 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,21 +1,26 @@
 # Your Code Here
-def map(array)
-
-array
-
-yield
-
+def map(source_array)
     i = 0
-    while i < array.length do
-      array[i] = array[i] * -1  # <== Unique work
+    while i < source_array.length do
+      source_array[i] = yield (source_array[i])
       i += 1
     end
-
-  yield
-
+    source_array
 end
 
 
-def reduce(array)
-  yield(array)
+def reduce(source_array, starting_point=0)
+  i = 0
+  value = starting_point
+
+  while i < source_array.length do
+    value = yield(source_array[i],value)
+    i += 1
+  end
+
+  if(value==nil)
+    value=false
+  end
+value
+
 end
