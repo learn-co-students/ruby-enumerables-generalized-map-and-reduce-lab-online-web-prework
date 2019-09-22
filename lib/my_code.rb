193dbe88032991d5d85ac6ce2904ae951fd665f3
Juan Bermudez <juan.bermudez1102@outlook.com>
2019-09-22 21:37:21 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
 # Your Code Here
+def map(array)
+
+array
+
+yield
+
+    i = 0
+    while i < array.length do
+      array[i] = array[i] * -1  # <== Unique work
+      i += 1
+    end
+
+  yield
+
+end
+
+
+def reduce(array)
+  yield(array)
+end
