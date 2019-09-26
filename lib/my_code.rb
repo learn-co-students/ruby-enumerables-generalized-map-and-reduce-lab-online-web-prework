02a4b1a13556ea4a2e8b864466cef4dd75c1ec98
Joseph Miller <jdeshaiseworldtv@gmail.com>
2019-09-26 03:16:22 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
-# Your Code Here
+def map(array)
+ #map return a new array of manipiputlated elemnts
+ new=[]
+ i = 0 
+ while i < array.length 
+ new.push yield(array[i]))
+  i+=1
+ end
+ new 
+end
+
+def reduce(array, sv=nil)
+  if sv
+ num = sv
+i=0
+else
+  num = array[0]
+  i =1
+end
+
+while i < array.length
+   sum =yield(sum, array[i])
+   i+= 1 
+  end
+   sum
+ end
