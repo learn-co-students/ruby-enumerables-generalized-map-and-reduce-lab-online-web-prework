83e6744a81c25071256019395fa5b97f3114671d
Laura Hatch <laura.hatch.lh@gmail.com>
2019-09-27 23:28:06 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,4 +8,17 @@ end
 negetive_array
 end
 
-def reduce (source_array)
+def reduce(array,sv = nil)
+  if sv
+    sum = sv
+      i = 0
+  else
+    sum = array[0]
+    i = 1
+end
+while i < array.length
+    sum = yield(sum,array[i])
+    i += 1
+end
+sum
+end
