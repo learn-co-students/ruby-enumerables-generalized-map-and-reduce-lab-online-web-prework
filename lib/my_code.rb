6f35e313cef2933d32679145903c03cc6925edcb
Ricky dildy <rdildy92@gmail.com>
2019-09-27 01:54:04 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
-# Your Code Here
+my_map = (source_array)
+
+{
+do 
+  source_array= ["1", "2", "3", "4", "5"]
+  [
+  # expect  ["1", "2", "3", "4", "5"]
+  my_map << ("6")
+  #expecting ("6").to = ["1", "2", "3", "4", "5", "6"]
+ 
+  source_array = ["1", "2", "3", "4", "5", "6"]
+ 
+  
+ 
+
+   
+
+
+source_array = ["1", "2", "3", "4", "5", "6"]
+#expecting(source_array).to.map
+source_array.map
+expecting 
+]
+end
+
+}
+
