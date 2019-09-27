23b24ef0c7b63a36860d9e6af57df7e5135ce682
Laura Hatch <laura.hatch.lh@gmail.com>
2019-09-27 22:12:45 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,11 @@
-# Your Code Here
+def map (source_array)
+  counter = 0
+  negetive_array = []
+while counter < source_array.length do
+  negetive_array << yield(source_array[counter])
+  counter += 1
+end
+negetive_array
+end
+
+def reduce (source_array)
