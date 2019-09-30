cab79b20c48a1822ab99de82ff67b3c1f74f4ffb
joshua richards <jermain119@gmail.com>
2019-09-30 00:59:40 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
-# Your Code Here
+#map returns a new array with manipulated values
+def map(source_array)
+  i= 0
+  negative_array =[]
+while i < source_array.count do
+  negative_array.push(yield (source_array[i]))
+  i += 1
+end
+negative_array
+end 
+
+def reduce(source_array, sp=nil)
+  if sp
+    total = sp
+    i = 0 
+  else 
+    total = source_array[0]
+    i = 1 
+  end
+  while i < source_array.length
+    total = yield(total, source_array[i])
+    i += 1 
+  end
+  total
+end 
