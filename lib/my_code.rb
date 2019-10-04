2d725a2fd1b6d5a7b2d7ab8a3f0d1003699c8cf3
Kevin Downs <kevin.downs04@gmail.com>
2019-10-04 09:22:25 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+
+def map(array)
+  new_array =[]
+  counter = 0
+  while counter < array.length do
+  new_array[counter] = yield(array[counter])
+  counter += 1
+  end
+  return new_array
+end
+
+def reduce(array, starting_value = nil)
+  value = starting_value
+  counter = 0
+  if value == nil
+    value = array[0]
+    counter += 1
+  end
+  while counter < array.length do
+    value = yield(value, array[counter])
+    counter += 1
+  end
+  return value
+end
