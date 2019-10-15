e0420acdaa1e76aca01451f771f5ef0af181c1bd
Romario Gustave <romagustave10@gmail.com>
2019-10-15 06:42:00 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,18 @@
 # Your Code Here
+def map()
+  map = [1,2,3,4]
+end
+
+def map_2_negative()
+  map = [1,2,3,4]
+map_2_negative = map *(-1)
+end
+
+def reduce() 
+  map = [1,2,3,4]
+  reduce == map[0]+map[1]+map[2]+map[3]
+end
+
+  puts map_2_negative
+   puts reduce
+ end
