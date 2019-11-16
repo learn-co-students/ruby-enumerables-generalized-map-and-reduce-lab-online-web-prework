e36026dd1933fc425db450045eb7ee6ab281483c
Benjamin Small <ben20262@aim.com>
2019-11-16 18:30:33 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,20 @@
-# Your Code Here
+def map(array)
+  new = []
+  array.each do |index|
+    new << yield(index)
+  end
+  new
+end
+
+def reduce(array, start = 0)
+  num = array[0]
+  if start != 0
+    num += start
+  end
+  count = 1
+  while count < array.length do
+    num = yield(num, array[count])
+    count += 1
+  end
+  num
+end
