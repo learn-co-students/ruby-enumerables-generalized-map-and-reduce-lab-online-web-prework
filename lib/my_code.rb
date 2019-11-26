d89b2753c898ee4f9dbf4bae7f2ecf0576470972
Justin Branch <justinms@outlook.com>
2019-11-26 04:01:39 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,44 @@
-# Your Code Here
+array = [1,2,3]
+
+def map(source_array)
+  i = 0
+  newarray = []
+  while i < source_array.length do
+    newarray << yield(source_array[i])
+    i += 1 
+  end
+newarray
+end
+
+map(array) {|n| n * -1}
+map(array) {|n| n}
+map(array) {|n| n * 2}
+map(array) {|n| n ** 2}
+
+
+def reduce(source_array, starting_point = 0)
+  i = 0
+  value = starting_point
+  while i < source_array.length do 
+    value = yield(source_array[i], value)
+    i += 1 
+  end
+value 
+end 
+
+truthy = [true, true, true, true]
+falsy = [false, false, false]
+any = [true, false, false, true]
+
+reduce(array) {|m, n| m + n}
+reduce(truthy).all? {|m, n| m}
+reduce(any).any? {|m, n| m} 
+
+
+
+
+
+
+
+
+  
