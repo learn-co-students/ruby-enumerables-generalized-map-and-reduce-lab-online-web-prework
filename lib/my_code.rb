b2e2535023b9fdaac6d907625d0847632527dda5
OLESYA MILLER <olesyam.miller@gmail.com>
2019-12-01 06:09:52 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
 # Your Code Here
+
+def map(array) 
+  final_array = []
+  counter = 0 
+  while counter < array.length do 
+    final_array.push(yield(array[counter]))
+    counter += 1 
+  end
+  final_array
+end
+
+def reduce(array, sv = nil)
+  if sv 
+    sum = sv 
+    i = 0 
+  else 
+    sum = array[0]
+    i = 1 
+  end 
+  while i < array.length do 
+    sum = yield(sum, array[i])
+    i += 1 
+  end
+  sum 
+end 
