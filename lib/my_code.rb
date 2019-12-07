1c37a0e3bb9ed82e68aef7554eb6513da115ba56
Marcel Rico <personal.mr4@gmail.com>
2019-12-07 00:12:10 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
 # Your Code Here
+
+def map(array)
+  results = []
+  array.length.times do |idx|
+    results << yield(array[idx])
+  end
+  results
+end
+
+def reduce(array,starting_value=nil)
+  
+  if(starting_value)
+    total = starting_value
+    i=0
+  else
+    total = array[0]
+    i=1
+  end
+  
+  while i < array.length do
+    total = yield(total,array[i])
+    i+=1
+  end
+  total
+end
+
+#[false,nil,nil,nil,true]
