166577715b4c92ffc0b1bd00ffe2fea7b8106797
Alex Engelhart <asengelhart@yahoo.com>
2019-12-08 02:20:18 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
 # Your Code Here
+def map(source)
+  result = []
+  i = 0 
+  while i < source.length do 
+    result << yield(source[i])
+    i += 1 
+  end 
+  result
+end 
+
+def reduce(source, start = nil)
+  if start 
+    result = start 
+    i = 0 
+  else 
+    result = source[0]
+    i = 1 
+  end 
+  while i < source.length do
+    result = yield(result, source[i])
+    if result == nil # accounts for (nil && false) == nil
+      result = false 
+    end 
+    i += 1 
+  end 
+  result
+end
+  
