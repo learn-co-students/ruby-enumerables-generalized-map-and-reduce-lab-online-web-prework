73bfe3030d54682ed23c009c00463a7c19daaa69
Dan Foley <dan@dan-foley.com>
2019-12-17 00:36:51 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,20 @@
 # Your Code Here
+def map(arr)
+  i = 0
+  new_arr = []
+  while i < arr.length do
+    new_arr << yield(arr[i])
+    i += 1
+  end
+  new_arr
+end
+
+def reduce(arr, start = 0)
+  i = 0
+  total = 0
+  while i < arr.length do
+    total += yield(arr[i])
+    i += 1
+  end
+  total
+end
